59a5b9efa3cd985a0bf3f7d2e5a37dae2a848ea9
tdregmans <tdregmans+github@protonmail.com>
2023-11-20 19:42:15 +0100

    Worked on TrigonometricFunctions and seperated special functions from easy calculations

--- AdvancedCalculator.py
+++ AdvancedCalculator.py
@@ -12,10 +12,8 @@ class AdvancedCalculator:
     def __str__(self):
         return self.name
     
-    def recursiveCalculation(self, calculation):
-        # regex = re.compile(r"\b(?P<single>[a-zA-Z0-9]+)\b")
-
-        regex = re.compile(r"(?P<first>[a-zA-Z0-9,.]+)\s*(?P<op>|\+|-|\*|/|%)\s*(?P<second>[a-zA-Z0-9.,]+)")
+    def simpleCalculation(self, calculation):
+        regex = re.compile(r"(?P<first>-?[a-zA-Z0-9,.]+)\s*((?P<op>|\+|-|\*|/|%)\s*(?P<second>-?[a-zA-Z0-9.,]+))?")
         group = regex.search(calculation)
         try:
             op = group.group("op")
@@ -31,9 +29,68 @@ class AdvancedCalculator:
                 return first / second
             elif op == "%":
                 return first % second
-        except AttributeError or ValueError: # a group is unknown
+            else:
+                print("ERROR!")
+                print("Unknown operator: {op}")
+                return None
+        except: # `op` - or `second` - group is unkown -> This means `first` is a single
             print("ERROR!")
+            return float(group.group("first"))
+        
+    def TrigonometricFunctions(self, calculation):
+        regex = re.compile(r".*(?P<op>(sin|sinh|cos|cosh|tan|tanh|log))\s*\(\s*(?P<content>(.*))\s*\).*")
+        group = regex.search(calculation)
+        if group["op"] == "sin":
+            return math.sin(float(self.simpleCalculation(group["content"])))
+        elif group["op"] == "sinh":
+            return math.sinh(float(self.simpleCalculation(group["content"])))
+        elif group["op"] == "cos":
+            return math.cos(float(self.simpleCalculation(group["content"])))
+        elif group["op"] == "cosh":
+            return math.cosh(float(self.simpleCalculation(group["content"])))
+        elif group["op"] == "tan":
+            return math.tan(float(self.simpleCalculation(group["content"])))
+        elif group["op"] == "tanh":
+            return math.tanh(float(self.simpleCalculation(group["content"])))
+        # Even though 'log' is not a Trigonometric Function, it is a three-letter functions, so in an attempt to create order, I put them together.
+        elif group["op"] == "log":
+            return math.log10(float(self.simpleCalculation(group["content"])))
+        else:
+            print("ERROR!")
+            return float(group["content"])
+        
+    
+    def recursiveCalculation(self, calculation):
+        # remove priority
+        regex = re.compile(r"\((?P<first>[\S\s]*)\)\s*((?P<op>|\+|-|\*|/|%)\s*(?P<second>-?[a-zA-Z0-9.,]+)|(?P<op1>|\+|-|\*|/|%)\s*(?P<second1>[\S\s]*))*")
+        # regex = re.compile(r"\((?P<first>.*)\)\s*((?P<op>|\+|-|\*|/|%)\s*(?P<second>-?[a-zA-Z0-9.,]+)|(?P<op1>|\+|-|\*|/|%)\s*(?P<second1>.*))*")
+        group = regex.findall(calculation) 
+        print(group)
 
+        return self.TrigonometricFunctions(calculation)
+        # op = group.("op")
+        # print(op)
+
+        # regex = re.compile(r"(?P<first>[a-zA-Z0-9,.]+)\s*(?P<op>|\+|-|\*|/|%)\s*(?P<second>[a-zA-Z0-9.,]+)")
+        # group = regex.search(calculation)
+        # try:
+        #     op = group.group("op")
+        #     first = float(group.group("first"))
+        #     second = float(group.group("second"))
+        #     if op == "+":
+        #         return first + second
+        #     elif op == "-":
+        #         return first - second
+        #     elif op == "*":
+        #         return first * second
+        #     elif op == "/":
+        #         return first / second
+        #     elif op == "%":
+        #         return first % second
+        # except AttributeError or ValueError: # a group is unknown
+        #     print("ERROR!")
+
+    # optional
         # if groups.group("op") == "+":
         #     print(groups.groupdict())
         #     return self.recursiveCalculation(groups.group("single")) + self.recursiveCalculation(groups.group("single"))
@@ -45,6 +102,50 @@ class AdvancedCalculator:
             # (?P<single>([a-zA-Z0-9]+))\s*|(?P<first>[a-zA-Z0-9]+)\s*(?P<op>(|\+|-|\*|/))\s*(?P<second>[a-zA-Z0-9]+)
 
             # \s*(?P<single>[a-zA-Z0-9]+)\s* *(?P<op>[+|-|*|/])*
+        
+        '''
+        needed functions:
+            simple calculations:                \((?P<first>.*)\)\s*((?P<op>|\+|-|\*|/|%)\s*(?P<second>-?[a-zA-Z0-9.,]+)|(?P<op1>|\+|-|\*|/|%)\s*(?P<second1>.*))*
+                x + y
+                x - y
+                x * y
+                x / y
+            other calculations:   Niet getest   \((?P<first>.*)\)\s*((?P<op>|\+|-|\*|/|%|**)\s*(?P<second>-?[a-zA-Z0-9.,]+)|(?P<op1>|\+|-|\*|/|%)\s*(?P<second1>.*))*
+                x % y
+                x ** y
+            arthritmic functions:               .*(?P<op>(sin|cos|tan|log))\s*\(\s*(?P<content>(.*))\s*\).*
+                sin x
+                cos x
+                tan x
+                log x
+            priority:
+                (x + y) * z
+                x + (y / z)
+            combinations:
+                x + (sin y / z)
+            sets:
+                {x, y, z}
+            constants:
+                e
+                pi
+            plots:
+                functions
+                tables
+                traces
+
+            others:
+                sum
+                product
+                ABC-formula
+                absolute value
+                rounding
+                remainder
+                max
+                min
+                factorial
+                
+
+        '''
 
 
     def saveVar(self, calculation):
@@ -65,7 +166,7 @@ class AdvancedCalculator:
             self.hist.clear()
         else:
             self.hist.append(self.recursiveCalculation(calculation))
-            print(self.hist)
+            print(self.hist[-1])
 
 
 # regex:
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ An advanced calculator, powered by Python
 
 **Thijs Dregmans**
 
-Last updated on 2023-11-15
+Last updated on 2023-11-20
 
 ## Course
 
@@ -13,5 +13,36 @@ This Python calculator is made for a bonus course, in the Technische Informatica
 
 The goal of this calculator is, both to learn more about Object Oriented Programming in Python, and to complete the course.
 
+## Installations
+
+The calculator is written in Python. Python is a interpreter language. This means, you need install Python.
+This calculator is specifically designed for and built in 'Python 3.11.1'. 
+
 ## Architecture
 
+In `main.py`, a `AdvancedCalculator`-object is created. The user input is fed to this object with the `calc()` method.
+In the `AdvancedCalculator.calc()` method, the user input is validated and scanned for calculations and/or commands.
+
+This method then calls other methods and carries out the tasks that need to be done.
+
+## User manual
+
+### Commands
+
+The user has several commands, he/she can use:
+
+- `help` provides the user with information about the available commands.
+- `quit` stops the calculator.
+- `saveAs` saves the previous value to a temporary memory cell. This command takes the name of the variable as argument.
+- `erase` erases the content of the memory cells.
+
+### Calculations
+
+A calculator is useless, if it cannot perform calculations.
+This calculator accepts the most common operators:
+
+- `+` adds two arguments. (infix operator)
+- `-` substracts two arguments. (infix operator)
+- `*` multiplies two arguments. (infix operator)
+- `/` divides two arguments. (infix operator)
+- `%` calculates the mode of two arguments. (infix operator)
